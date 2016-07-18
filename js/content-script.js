e8b9bf2201d447977a1b80ccd421a86b16e9be49
yamap55 <tetuya120@hotmail.com>
2016-07-18 14:39:20 +0900

    設定ボタン追加。

--- js/content-script.js
+++ js/content-script.js
@@ -115,6 +115,8 @@ var getTableData = function () {
   return data;
 };
 var tableData = getTableData();
+
+// EhrHelper
 var EhrHelper = function(data){
   this.data = data;
 };
@@ -193,6 +195,7 @@ EhrHelper.prototype.closeButton =  $('<a>').attr({"id":"closeBtn","href":"#"}).t
   'cursor': 'pointer',
   'font-size': '150%',
   'text-decoration': 'none',
+  'float':'right',
 });
 
 // 操作領域追加。
@@ -264,6 +267,120 @@ EhrHelper.prototype.addCommentTemplate = function (){
   commentArea.parent().append(selectElem);
 };
 
+// 設定窓
+EhrHelper.prototype.settingWindow = $('<div>').css({
+  'position': 'fixed',
+  'top': '10%',
+  'left': '10%',
+  'width': '80%',
+  'background-color': 'gray'
+ }).attr({
+  'id': 'settingWindow'
+});
+
+// 設定窓の設定を行う。
+EhrHelper.prototype.settingWindowSetting = function(){
+  // Closeボタン
+  var close = $('<input>').attr({
+    'type': 'button',
+    'value': 'CLOSE'
+  }).on('click', function () {
+    $("#settingWindow").hide('fast');
+  });
+
+  // 保存ボタン
+  var save = $('<input>').attr({
+    'type': 'button',
+    'value': '保存'
+  }).on('click', function () {
+    // TODO
+    // var data = getSettingData();
+    // // 取得したdataを保存
+    // localStorage.setItem(KEY, JSON.stringify(data));
+    // console.log("保存しました。");
+    // console.log(JSON.stringify(data));
+    // console.log(data);
+  });
+
+  // テーブルのCSS
+  var tablecss = {
+    'border-collapse': 'collapse',
+    'border': '1px solid #333'
+  };
+
+  // 設定テーブル
+  var table = $('<table>',{'id':'settingTable'}).css(tablecss);
+
+  // tr作成関数。
+  var createTr = function (viewName, projectName, taskName, time, thFlag) {
+    if (thFlag) {
+      var td = $('<th>').css(tablecss);
+      var v = td.clone().text(viewName);
+      var p = td.clone().text(projectName);
+      var t = td.clone().text(taskName);
+      var ti = td.clone().text(time);
+      var h = td.clone().text("-");
+      return $('<tr>',{'id':'tablehead'}).css(tablecss).append(v, p, t, ti,h);
+    } else {
+      var resultTr = $('<tr>').css(tablecss);
+      var createTd = function(str) {
+        td = $('<td>').css(tablecss);
+        $('<input>').attr({'type':'text'}).val(str).appendTo(td);
+        return td;
+      }
+      var v = createTd(viewName);
+      var p = createTd(projectName);
+      var t = createTd(taskName);
+      var ti = createTd(time);
+      var closeTd = $('<td>').css(tablecss).append($('<input>').attr({'type':'button','value':'×'}).css('width','20px').on('click',function(){resultTr.remove();}));
+      return resultTr.append(v, p, t, ti,closeTd);
+    }
+  };
+
+  // 設定窓下部メッセージ
+  var messageArea = $('<ul>');
+  [
+    "表示名 : 表示される名称。",
+    "プロジェクト名 : プロジェクトの作業名称。時間入力する所の値をそのままコピーしてください。「（」などもコピーしてください。",
+    "タスク名 : ↑のプロジェクト名の「+」を押した中にある項目。数値もそのままコピーしてください。",
+    "時間 : 設定される時間。「00:15」や「01:00」のような形式で入力してください。"
+  ].forEach((s)=>{messageArea.append($('<li>').text(s))});
+  // ヘッダの作成
+  table.append(createTr('表示名', 'プロジェクト名', 'タスク名', '時間', true));
+
+  // データを復旧。
+  $.each(Util.getSaveData().settingList,function(i,d) {
+    table.append(createTr(d[0], d[1], d[2], d[3]));
+  });
+
+  // 入力欄追加ボタン
+  var addTrBtn = $('<input>').attr({
+    'type': 'button',
+    'value': '入力欄追加'
+  }).on('click', function () {
+    table.append(createTr('', '', '', ''));
+  });
+
+  this.settingWindow.append(close,save,addTrBtn,table,messageArea);
+};
+
+// 設定ボタンを追加する。
+EhrHelper.prototype.addSettingWindowButton = function (){
+  this.settingWindowButton.prependTo(this.operationContainer);
+  this.settingWindow.hide();
+  $('body').append(this.settingWindow);
+  this.settingWindowSetting();
+};
+
+// 設定ボタン
+EhrHelper.prototype.settingWindowButton = $('<input type=\'button\'>').attr({
+  'id': 'setting_btn',
+  'value': '設定'
+}).on('click', function () {
+  // 設定窓の表示非表示の切り替え。
+  $("#settingWindow").toggle('fast');
+});
+
 var ehrHelper = new EhrHelper(tableData);
 // TODO 設定で有効有無を切り替えられるようにする。
 ehrHelper.timeClockNotification();
@@ -272,3 +389,4 @@ ehrHelper.addOperationContainer();
 ehrHelper.addClosebutton();
 ehrHelper.addSettingData();
 ehrHelper.addCommentTemplate();
+ehrHelper.addSettingWindowButton();
