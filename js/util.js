ef765d5eb73e745fc6935d902c7c58b487b95954
yamap55 <tetuya120@hotmail.com>
2016-08-02 22:41:31 +0900

    共通処理にコメントを追加。

--- js/util.js
+++ js/util.js
@@ -28,34 +28,37 @@ var DEFAULT_COMMENT_TEMPLATES = {
   "家庭事情":"家庭事情のため遅刻。"
 };
 
+// 共通処理
 var Util = {
+  // 「00:00」、「0000」形式の文字列を分に変換する。
+  // 「000」など3桁はNG。
   parseMinutes: function (num) {
-    // 「00:00」、「0000」形式の文字列を分に変換する。
-    // 「000」など3桁はNG。
-    if (num == '') {
+    if (!num) {
       return 0;
     }
     var m = num.match(/^(\d\d):?(\d\d)$/);
     return parseInt(m[1]) * 60 + parseInt(m[2]);
   },
+  // 分を「0000」形式に変換する。
   parse0000 : function (num) {
-    // 分を「0000」形式に変換する。
     return num <= 0 ? '0000':('0' + (num / 60 | 0)).slice( - 2) + ('0' + (num % 60)).slice( - 2);
   },
+  // 該当のテキストボックスに時間を設定する。
   setTimeForText : function (textbox, time) {
     // 設定値を反映するため、閉じるボタンにfocusをあてることで既存の関数を呼び出す。
     textbox.val(time);
     $("#targetIdArea").val(textbox.attr("id"));
     $("#closeBtn").focus().blur();
   },
+  // 指定されたデータをlocalStorageに保存する。
   saveData : function(data) {
     console.log(data);
     // 取得したdataを保存
     localStorage.setItem(KEY, JSON.stringify(data));
     console.log(JSON.parse(localStorage.getItem(KEY)));
   },
+  // localStorageに保存されたデータを取得する。
   getSaveData : function() {
-    // 保存したdataを取得。
     if (localStorage.getItem(KEY)) {
       return JSON.parse(localStorage.getItem(KEY));
     }
@@ -66,6 +69,7 @@ var Util = {
       commentTemplates : DEFAULT_COMMENT_TEMPLATES,
     };
   },
+  // 残時間を全て、指定されたテキストボックスに設定する。
   setAllTime : function (targetTextBox) {
     var time = this.parseMinutes(targetTextBox.val());
     var remainingTime = $('#calc_attend_2_project_hours').text();
